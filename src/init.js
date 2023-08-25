3c77f25d4a4a250925b9e3303d8b87cf77b477e5
Dmitrii Bedov <dmitrii.bedov@idealo.de>
2023-08-25 17:06:13 +0200

    Add yup validation of url

--- src/init.js
+++ src/init.js
@@ -1,13 +1,14 @@
 // @ts-check
 
 import Example from './Example.js';
-
 import onChange from 'on-change';
+import * as yup from 'yup';
 
 export default () => {
   const elements = {
-    searchBar: document.getElementsByClassName('form-floating'),
+    searchBar: document.getElementById('url-input'),
     buttonAdd: document.getElementsByClassName('btn-primary'),
+    feedback: document.getElementsByClassName('feedback'),
   }
 
   const intitalState = {
@@ -16,12 +17,41 @@ export default () => {
       state: 'filling',
     },
   };
+  
 
-  const state = onChange(intitalState, () => {
+  // Model
+  const state = onChange(intitalState, (path, value, previouValue) => {
+      console.log('stateChange', path, value, JSON.stringify(state));
+      if (path === 'form.state') {
+        renderError(state);
+          // render Error
+      }
+  })
+ 
+  
+  // View
+  const renderError = (state) => {
+    elements.feedback[0].innerHTML = 'Please use a valid RSS URL';
+  };
 
-  });
 
-  console.log('El', elements)
-  // const obj = new Example(element);
-  // obj.init();
+  // Controller
+  elements.searchBar?.addEventListener('input', (e) => {
+      state.form.state = 'filling';
+      const url = state.form.url = e.target?.value;
+      // @TODO Validate
+
+      const schema = yup.string().url(url);
+      console.log('schema', schema);
+
+      const isValidUrl = (schema) => {
+        if (schema.StringSchema = false) {
+          state.form.state = 'error'
+          console.error(schema);
+        } state.form.state = 'valid'
+      }
+
+      console.log('urlValidation', isValidUrl);
+
+  })
 };
